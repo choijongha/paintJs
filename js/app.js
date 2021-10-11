587b5ff417e2c83df33e727c91c55ff49bbfb127
choijongha <80575094+choijongha@users.noreply.github.com>
2021-10-11 22:37:48 +0900

    oh?

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const width = document.getElementById("jsRange");
+// const colors = document.getElementsByClassName("controls__color")
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = width.value;
 
 let painting = false;
-
+let filling = false;
 // function stopPainting() {
 //     painting = false;
 // }
@@ -37,10 +38,6 @@ let painting = false;
 //     canvas.addEventListener("mouseup", stopPainting);
 //     canvas.addEventListener("mouseleave", stopPainting);
 // }
-
-$("#jsRange").change(function(event){
-    ctx.lineWidth = width.value;
-})
 $("#jsCanvas").mousemove(function (event) {
     const x = event.offsetX;
     const y = event.offsetY;
@@ -52,39 +49,37 @@ $("#jsCanvas").mousemove(function (event) {
         ctx.stroke();
     }
     });
-$("#jsCanvas").mousedown(function (event) {
+
+$("#jsCanvas").mousedown(function () {
+        painting = false;
+    });
+$("#jsCanvas").mouseup(function() {
         painting = true;
     });
-$("#jsCanvas").mouseup(function(event) {
-        painting = false;
-    });
-$("#jsCanvas").mouseleave(function(event) {
+$("#jsCanvas").mouseleave(function() {
         painting = false;
     })
-$(".controls__color:nth-child(1)").click(function(event) {
-    ctx.strokeStyle = "white";
+
+
+// function changeColor(event) {
+//     ctx.strokeStyle = event.target.style.backgroundColor;
+// }
+
+// Array.from(colors).forEach(color => color.addEventListener("click" , changeColor));
+
+$(".controls__color").click(function(event) {
+    ctx.strokeStyle = event.target.style.backgroundColor;
 })
-$(".controls__color:nth-child(2)").click(function(event) {
-    ctx.strokeStyle = "#ff3b30";
+$("#jsRange").change(function(event){
+    ctx.lineWidth = event.target.value;
 })
-$(".controls__color:nth-child(3)").click(function(event) {
-    ctx.strokeStyle = "#ff9500";
-})
-$(".controls__color:nth-child(4)").click(function(event) {
-    ctx.strokeStyle = "#ffcc00";
-})
-$(".controls__color:nth-child(5)").click(function(event) {
-    ctx.strokeStyle = "#4cd963";
-})
-$(".controls__color:nth-child(6)").click(function(event) {
-    ctx.strokeStyle = "#5ac8fa";
-})
-$(".controls__color:nth-child(7)").click(function(event) {
-    ctx.strokeStyle = "#0579ff";
-})
-$(".controls__color:nth-child(8)").click(function(event) {
-    ctx.strokeStyle = "#5856d6";
-})
-$(".controls__color:nth-child(9)").click(function(event) {
-    ctx.strokeStyle = "#2c2c2c";
+$("#jsMode").click(function() {
+    if(filling === false) {
+        $("#jsMode").html("paint");
+        filling = true;
+    } else {
+        $("#jsMode").html("fill")
+        filling = false;
+    }
+
 })
